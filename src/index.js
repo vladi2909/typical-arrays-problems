71ef94291786c364b8781e692f227ae929a98152
vladimir <vladi2909@outlook.com>
2021-02-09 20:05:14 +0300

    feat: add basic functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || !array.length) {
+    return 0;
+  }
+
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || !array.length) {
+    return 0;
+  }
+
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || !array.length) {
+    return 0;
+  }
+
+  return array.reduce((a, b) => a + b) / array.length;
 }
